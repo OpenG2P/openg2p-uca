cb971270245fad4831d23543f2aed9c9f380f291
Lalith Kota <kotalalith@gmail.com>
2025-05-07 15:02:07 +0530

    Clearing thread_id cookie on logout.
    Signed-off-by: Lalith Kota <kotalalith@gmail.com>

--- src/openg2p_uca/controllers/auth.py
+++ src/openg2p_uca/controllers/auth.py
@@ -1,3 +1,4 @@
+from fastapi import Response
 from openg2p_fastapi_auth.controllers.auth_controller import AuthController as BaseAuthController
 from openg2p_fastapi_auth.models.orm.login_provider import LoginProvider
 
@@ -18,3 +19,7 @@ class AuthController(BaseAuthController):
     async def get_login_provider_db_by_iss(self, iss: str) -> LoginProvider:
         ap = await AuthOauthProviderORM.get_auth_provider_from_iss(iss)
         return ap.map_auth_provider_to_login_provider() if ap else None
+
+    async def logout(self, response: Response):
+        await super().logout(response)
+        response.delete_cookie(_config.thread_id_cookie_name)
